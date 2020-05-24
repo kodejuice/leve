56e503facd22a9a92d3a5ad205efa160afec2544
kodejuice <kodejuice@gmail.com>
2020-05-24 14:37:32 +0100

    theme toggling

---
+++ components/home/Toggle.js
@@ -0,0 +1,31 @@
+import { parseCookies, setCookie } from 'nookies'
+import { Toggle as _Toggle } from "react-toggle-component"
+
+import {setTheme} from '../../utils';
+
+
+export default function Toggle(props) {
+	return (
+		<_Toggle
+	    leftBackgroundColor="#ccc"
+	    rightBackgroundColor="#333"
+	    borderColor="#282c34"
+	    knobColor="white"
+			name="t-3"
+			radius="3px"
+			radiusBackground="2px"
+			knobRadius="2px"
+			checked={+parseCookies(null).__dark == 1}
+			onToggle={e=>{
+				let {checked} = e.target;
+				setCookie(null, "__dark", (checked ? 1 : 0), {
+					path: '/',
+					maxAge: 86400 * 86400
+				});
+				setTheme(parseCookies(null).__dark);
+			}}
+		/>
+	);
+}
+
+
