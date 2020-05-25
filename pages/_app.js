85cf1828d600c5ffff684e1d68407535687ae1b6
kodejuice <kodejuice@gmail.com>
2020-05-25 13:50:46 +0100

    nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,15 @@ import '../assets/dark-theme.scss'
 import '../assets/pagination.scss'
 import '../assets/admin.scss'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Router from 'next/router'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
 
-// This default export is required in a new `pages/_app.js` file.
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function MyApp({ Component, pageProps }) {
 
     // add style <body> tag
