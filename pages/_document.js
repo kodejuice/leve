a3d6a290412d69968a1ea7899cc7b0dee006b6c2
kodejuice <kodejuice@gmail.com>
2020-11-28 09:45:58 +0100

    process.env.HOST

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,10 @@ class MyDocument extends Document {
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-                <link rel="canonical" href="https://kodejuice.now.sh" />
+                <link rel="canonical" href={`https://${process.env.HOST}`} />
                 <link rel="manifest" href="/favicon/site.webmanifest"/>
-                <meta name="referrer" content="no-referrer-when-downgrade" />
                 <link rel="alternate" type="application/rss+xml" title="Sochima Biereagu" href={`https://${process.env.HOST}/api/rss.xml`} />
+                <meta name="referrer" content="no-referrer-when-downgrade" />
              </Head>
             <body className="body">
                 <Main />
